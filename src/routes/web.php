32720888251f75cd8aac3fde7423bb8540530920
hiratemiyu <miyuhirate@gmail.com>
2023-06-20 00:43:09 +0900

    a

--- src/routes/web.php
+++ src/routes/web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
+| routes are loaded by the RouteServiceProvider and all of them willz
 | be assigned to the "web" middleware group. Make something great!
 |
 */
@@ -26,6 +26,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/index', function () {
+    return view('index');
 });
 
 require __DIR__.'/auth.php';
